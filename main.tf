29448034696c568bd2eb9000d788b20f5a56da54
dhruv-garg <dhruvgarg074@gmail.com>
2023-04-03 21:08:05 +0530

    Add provider version

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.0.0"
+    }
+  }
+}
+
 provider "aws" {
+  region = "us-east-1"
   assume_role {
     role_arn     = "arn:aws:iam::430288206927:role/TerraformTestRole"
     session_name = "TestSession"
