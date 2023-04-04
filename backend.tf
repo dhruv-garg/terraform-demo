cc1a6a861055a70200a994fc069e273bd0a8674a
dhruv-garg <dhruvgarg074@gmail.com>
2023-04-04 12:00:36 +0530

    moved backend to main tf file

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    encrypt        = true
-    bucket         = "dhruvgarg074-state"
-    dynamodb_table = "terraform-state-lock-dynamo"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.0.0"
     }
   }
+  backend "s3" {
+    encrypt        = true
+    bucket         = "dhruvgarg074-state"
+    dynamodb_table = "terraform-state-lock-dynamo"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+  }
 }
 
 provider "aws" {
