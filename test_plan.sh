d0b09081fb0e40c4b83377194c7779cf0dc162bf
dhruv-garg <dhruvgarg074@gmail.com>
2023-04-04 17:09:04 +0530

    Minor changes to script + renamed it

--- test_plan.sh
+++ test_plan.sh
@@ -3,5 +3,8 @@ output=`terraform plan`
 echo $output
 sub="No changes"
 if [[ "$output" == *"$sub"* ]]; then
-	echo "Woahh"
+	echo "No changes to terraform infrastructure"
+else
+	echo "Drift in terraform infrastructure"
+	exit 1
 fi
